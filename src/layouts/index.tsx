28c46591782c7eddf4cec29cca5ce00864d60486
lcl <2251086272@qq.com>
2026-05-07 23:52:42 +0800

    feat: 学习长任务与看performance的火焰图

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -6,7 +6,8 @@ import { Link, Outlet } from "umi";
 import "./index.less";
 import { useEffect, useState } from "react";
 import { Button } from "antd";
-
+import Index from "@/moment的笔记学习/前端工程化/07. 前端性能指标/长任务";
 export default function Layout() {
-  return <Outlet />;
+  // return <Outlet />;
+  return <Index></Index>;
 }
---
+++ src/moment的笔记学习/前端工程化/07. 前端性能指标/长任务.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  for (let i = 0; i < 1e9; i++) {
+    // 做一些操作
+  }
+  console.log("------------>");
+  return <div>长任务</div>;
+};
+
+export default Index;
